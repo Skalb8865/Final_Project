6ea2b13c3fae4cafb24532a60d07c1ab001433c4
Skalb8865 <ps25skalb@efcts.us>
2024-04-30 09:55:37 -0400

    dada

--- js/hamburger_menu.js
+++ js/hamburger_menu.js
@@ -1,3 +1,6 @@
-function toggleMobileMenu(menu){
-    menu.classList.toggle('open');
-}
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () =>{
+    navbarLinks.classList.toggle('active')
+})
